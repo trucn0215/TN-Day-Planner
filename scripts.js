1c5175e3060647c8e5c470d701776a667ff9d6b0
Truc Nguyen <trucn0215@gmail.com>
2020-11-02 18:18:30 -0800

    psuedocode

--- scripts.js
+++ scripts.js
@@ -3,3 +3,23 @@ var today = moment();
 $("#currentDay").text(today.format("dddd, MMMM Do"));
 
 // timeBlock
+var bussinessHours = ["9AM", "10AM", "11AM", "12PM", "1PM", "2PM", "3PM", "4PM", "5PM"];
+
+// Get the current hour of the day using moment.js
+var currentHour = moment().format("A");
+
+console.log(currentHour);
+// loop over the hours from 9AM to 5PM.
+
+//      var i = 9
+//      Element id = "hour-" + i
+//      Currently checking 9AM <currentHour
+//      9am is in the past
+
+//      Option A: Select matching element - $(Element id).CSS( "background");
+
+//      Option B: Select matching element - $(Element id).CSS( "background");
+
+// Add a click event ON A PARENT ELEMENT that can listen to my save buttons
+
+// 
