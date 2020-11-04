a26f0366d558584a3723bdb2ce00b23a2217fd37
Truc Nguyen <trucn0215@gmail.com>
2020-11-03 21:11:43 -0800

    if/else statement to highlight the text area to tell user that the time is past, current or future

--- scripts.js
+++ scripts.js
@@ -5,78 +5,54 @@ var saveIcon;
 var singleTimeBlock;
 
 var timeBlock = $(".time-Block")
+var textAreaHour = [];
+
+// Get the current hour of the day using moment.js
+var currentHour = parseInt(moment().format("H"));
+
+// timeBlock
+var bussinessHours = ["9AM", "10AM", "11AM", "12PM", "1PM", "2PM", "3PM", "4PM", "5PM"];
+var hoursID = [15, 16, 17, 18, 19, 20, 21, 22, 23];
 
 // adding moment js for today date in the header
 var today = moment();
 $("#currentDay").text(today.format("dddd, MMMM Do"));
 
-// Get the current hour of the day using moment.js
-var currentHour = parseInt(moment().format("k"));
-
-// timeBlock
-var bussinessHours = ["9AM", "10AM", "11AM", "12PM", "1PM", "2PM", "3PM", "4PM", "5PM"];
-
 // Create time blocks for from 9AM to 5PM
-function createTimeBlocks (){
-    for (var i=0; i < bussinessHours.length; i++){
+function createTimeBlocks() {
+    for (var i = 0; i < bussinessHours.length; i++) {
         singleTimeBlock = $("<div>").addClass("row");
         timeBlock.append(singleTimeBlock);
         hourEl = $("<div>").addClass("hour col text-right").text(bussinessHours[i]);
-        textEl = $("<textarea>").addClass("description col-10");
+        textEl = $("<textarea>").addClass("description col-10").attr("id", hoursID[i]);
         saveEl = $("<button>").addClass("saveBtn col text-center");
         singleTimeBlock.append(hourEl, textEl, saveEl);
 
         // Add save icon to save buttons
-        saveIcon = $("<i>").addClass("fa fa-save").css("font-size","36px");
+        saveIcon = $("<i>").addClass("fa fa-save").css("font-size", "36px");
         saveEl.append(saveIcon);
+
+
+        textAreaHour[i] = $("#" + hoursID[i]);
+        console.log(textAreaHour[i].attr("data-index"))
     }
-
-    // if (bussinessHours.length == currentHour){
-    //     textEl.addClass("present")
-    // }
-    // else if (bussinessHours.length < currentHour){
-    //     textEl.addClass("past")
-    // }
-    // else if (bussinessHours.length > currentHour){
-    //     textEl.addClass("future")
-    // }
-// console.log(bussinessHours.length)
-
 }
-// compare = bussinessHours[1] >= currentHour;
-// console.log(currentHour)
-// console.log(bussinessHours[i])
-// console.log(compare)
 
-// function timeCheck (){
-//     for (var i=0; i<bussinessHours.length+1;i++){
-//         if (bussinessHours[i] = currentHour){
-//             textEl.addClass("present")
-//         }
-//         else if (bussinessHours[i] < currentHour){
-//             textEl.addClass("past")
-//         }
-//         else {
-//             textEl.addClass("future")
-//         }
-//     }
-// }
+// check for the bussinessTime match with current time. If past current time, turn grey. if current, turn orange. If future, turn green.
+function timeCheck (){
+    for (var i=0; i < hoursID.length; i++){
+
+        if (hoursID[i] < currentHour){
+            textAreaHour[i].addClass("past")
+        }
+        else if (hoursID[i] == currentHour){
+            textAreaHour[i].addClass("present")
+        }
+        else if (hoursID[i] > currentHour){
+            textAreaHour[i].addClass("future")
+        }
+    }
+}
 
 createTimeBlocks();
-// timeCheck();
-// loop over the hours from 9AM to 5PM.
-
-//      var i = 9
-//      Element id = "hour-" + i
-//      Currently checking 9AM <currentHour
-//      9am is in the past
-
-//      Option A: Select matching element - $(Element id).CSS( "background");
-
-//      Option B: Select matching element - $(Element id).CSS( "background");
-
-// Add a click event ON A PARENT ELEMENT that can listen to my save buttons
-
-// class="hour col text-right"
-// class="description col-10"
-// class="saveBtn col text-center"
+timeCheck();
